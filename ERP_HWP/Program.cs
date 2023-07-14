c9fe693a9f7c562272f797f426101ce5a86fb425
quochao981 <98982598+quochao981@users.noreply.github.com>
2023-07-14 09:58:00 -0700

    Update Program.cs

--- ERP_HWP/Program.cs
+++ ERP_HWP/Program.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Program
+public class SaleOrder : FinishedGood
 {
-    private static List<SalesOrder> salesOrders = new List<SalesOrder>();
+    public string OrderID { get; set; }
+    public string CustomerID { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public int OrderStatus { get; set; }
+    public DateTime Deadline { get; set; }
+    public List<FinishedGood> FinishedGood { get; set; }
+}
+
+public class FinishedGood
+{
+    public int GoodID { get; set; }
+    public int Status { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime DeadLine { get; set; }
+    public bool ProductCode { get; set; }
+    public int Quantity { get; set; }
+
+}
+
+public class Programs
+{
+    private static List<SaleOrder> salesOrders = new List<SaleOrder>();
     private static int soNumberCounter = 1;
 
     public static void Main()
@@ -33,7 +55,7 @@ public class Program
                     exit = true;
                     break;
                 default:
-                    Console.WriteLine("Invalid choice. Please try again.");
+                    Console.WriteLine("Invalid choice. Please choose another number.");
                     break;
             }
 
@@ -41,11 +63,12 @@ public class Program
         }
     }
 
+
+
     public static void CreateSalesOrder()
     {
-        SalesOrder salesOrder = new SalesOrder();
-        salesOrder.OrderNumber = GenerateSalesOrderNumber();
-
+        SaleOrder salesOrder = new SaleOrder();
+        salesOrder.OrderID = GenerateSalesOrderNumber();    
         Console.WriteLine("Enter finished goods information for the sales order (press Enter to finish):");
 
         bool addFinishedGoods = true;
@@ -53,7 +76,7 @@ public class Program
         {
             FinishedGood finishedGood = new FinishedGood();
 
-            Console.Write("Product Code: ");
+            Console.WriteLine("Product Code: ");
             finishedGood.ProductCode = Console.ReadLine();
 
             Console.Write("Quantity: ");
@@ -68,7 +91,7 @@ public class Program
                 continue;
             }
 
-            salesOrder.FinishedGoods.Add(finishedGood);
+            salesOrder.FinishedGood.Add(finishedGood);
 
             Console.Write("Add another finished good? (Y/N): ");
             string choice = Console.ReadLine();
@@ -76,8 +99,8 @@ public class Program
             Console.WriteLine();
         }
 
-        salesOrders.Add(salesOrder);
-        Console.WriteLine("Sales order created successfully. Order number: " + salesOrder.OrderNumber);
+        salesOrder.Add(salesOrder);
+        Console.WriteLine("Sales order created successfully. Order number: " + salesOrder.OrderID);
     }
 
     public static void ViewSalesOrders()
@@ -91,9 +114,9 @@ public class Program
         Console.WriteLine("Existing Sales Orders:");
         foreach (var salesOrder in salesOrders)
         {
-            Console.WriteLine("Order Number: " + salesOrder.OrderNumber);
+            Console.WriteLine("Order Number: " + salesOrder.OrderID);
             Console.WriteLine("Finished Goods:");
-            foreach (var finishedGood in salesOrder.FinishedGoods)
+            foreach (var finishedGood in salesOrder.FinishedGood)
             {
                 Console.WriteLine("- Product Code: " + finishedGood.ProductCode);
                 Console.WriteLine("  Quantity: " + finishedGood.Quantity);
@@ -107,129 +130,5 @@ public class Program
         string orderNumber = soNumberCounter.ToString("D5");
         soNumberCounter++;
         return orderNumber;
-    }
-}
-
-*****************************************************************************************************************************
-using System;
-using System.Collections.Generic;
-
-public class Programs
-{
-    private static List<SaleOrder> saleOrders = new List<SaleOrder>();
-    private static int currentSONumber = 1;
-
-    public static void Main(string[] args)
-    {
-        bool isRunning = true;
-
-        while (isRunning)
-        {
-            Console.WriteLine("1. Create Sales Order");
-            Console.WriteLine("2. View Sales Order");
-            Console.WriteLine("3. Exit");
-            Console.Write("Enter your choice: ");
-
-            string choice = Console.ReadLine();
-
-            switch (choice)
-            {
-                case "1":
-                    CreateSalesOrder();
-                    break;
-                case "2":
-                    ViewSalesOrder();
-                    break;
-                case "3":
-                    isRunning = false;
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
-            }
-
-            Console.WriteLine();
-        }
-    }
-
-    private static void CreateSalesOrder()
-    {
-        SaleOrder saleOrder = new SaleOrder();
-        saleOrder.SONumber = currentSONumber++;
-
-        Console.Write("Enter Customer ID: ");
-        saleOrder.CustomerID = Console.ReadLine();
-
-        Console.Write("Enter Order Status: ");
-        saleOrder.OrderStatus = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter Deadline (yyyy-mm-dd): ");
-        saleOrder.Deadline = DateTime.Parse(Console.ReadLine());
-
-        Console.Write("Enter the number of Finished Goods: ");
-        int numFinishedGoods = int.Parse(Console.ReadLine());
-
-        saleOrder.FinishedGoods = new List<FinishedGood>();
-
-        for (int i = 0; i < numFinishedGoods; i++)
-        {
-            FinishedGood finishedGood = new FinishedGood();
-
-            Console.Write("Enter Good ID: ");
-            finishedGood.GoodID = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter Status: ");
-            finishedGood.Status = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter Created Date (yyyy-mm-dd): ");
-            finishedGood.CreatedDate = DateTime.Parse(Console.ReadLine());
-
-            Console.Write("Enter Deadline (yyyy-mm-dd): ");
-            finishedGood.DeadLine = DateTime.Parse(Console.ReadLine());
-
-            Console.Write("Is it Bulk? (true/false): ");
-            finishedGood.IsBulk = bool.Parse(Console.ReadLine());
-
-            Console.Write("Enter Quality: ");
-            finishedGood.Quality = int.Parse(Console.ReadLine());
-
-            saleOrder.FinishedGoods.Add(finishedGood);
-        }
-
-        saleOrders.Add(saleOrder);
-
-        Console.WriteLine("Sales Order created successfully!");
-    }
-
-    private static void ViewSalesOrder()
-    {
-        Console.Write("Enter the Sales Order Number: ");
-        int soNumber = int.Parse(Console.ReadLine());
-
-        SaleOrder saleOrder = saleOrders.Find(so => so.SONumber == soNumber);
-
-        if (saleOrder != null)
-        {
-            Console.WriteLine("Sales Order Number: {0}", saleOrder.SONumber);
-            Console.WriteLine("Customer ID: {0}", saleOrder.CustomerID);
-            Console.WriteLine("Order Status: {0}", saleOrder.OrderStatus);
-            Console.WriteLine("Deadline: {0}", saleOrder.Deadline);
-
-            Console.WriteLine("Finished Goods:");
-            foreach (FinishedGood finishedGood in saleOrder.FinishedGoods)
-            {
-                Console.WriteLine("Good ID: {0}", finishedGood.GoodID);
-                Console.WriteLine("Status: {0}", finishedGood.Status);
-                Console.WriteLine("Created Date: {0}", finishedGood.CreatedDate);
-                Console.WriteLine("Deadline: {0}", finishedGood.DeadLine);
-                Console.WriteLine("Is Bulk: {0}", finishedGood.IsBulk);
-                Console.WriteLine("Quality: {0}", finishedGood.Quality);
-                Console.WriteLine();
-            }
-        }
-        else
-        {
-            Console.WriteLine("Sales Order not found!");
-        }
     }
 }
